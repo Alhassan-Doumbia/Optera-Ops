71e1530b4b9175c68ba7359459c0e965b5c534b8
Doumbia Alhassan <hassan4cod@gmail.com>
2024-10-23 11:13:47 +0000

    first commit & push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-svw h-svh items-center justify-center'>
+        <section className='w-[500px] h-[300px] brounded-md bg-gray-400'>
+
+        </section>
+    </div>)
+}
+
+export default App
---
+++ src/plaground.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function plaground() {
+  return (
+    <div className="w-svw h-svh items-center justify-center">
+      <section className="w-[500px] h-[300px] brounded-md bg-gray-400"></section>
+    </div>
+  );
+}
+
+export default plaground;
+
