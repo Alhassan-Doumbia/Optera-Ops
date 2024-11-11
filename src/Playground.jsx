c746e11cb882e6214334e41b8fb0383eb6e47ea6
Doumbia Alhassan <hassan4cod@gmail.com>
2024-11-11 18:44:20 +0000

    created LeaderBoard and updated curve handling function

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import OverView from "./view/OverView";
 function Playground() {
   return (
     <div className="w-svw h-svh items-center justify-center">
-      <section className="w-[500px] h-[300px] bg-blue-600">
+      <section className="w-[500px] min-h-[300px h-fit bg-blue-600">
             <Navbar></Navbar>
+            <OverView></OverView>
       </section>
     </div>
   );
