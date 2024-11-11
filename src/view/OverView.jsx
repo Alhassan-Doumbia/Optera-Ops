379687be8ab80fc3817381872153a5212a2ecb12
Doumbia Alhassan <hassan4cod@gmail.com>
2024-11-11 18:42:42 +0000

    integrated the curve element to show efficacity on average time by successfull cal

--- src/view/OverView.jsx
+++ src/view/OverView.jsx
@@ -1,45 +1,52 @@
 import React, { useRef, useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
 import { updateCurveChart } from "../Hooks/handleCurveCharts";
 
 function OverView() {
-    const curveChartRef=useRef(null);
-    const profile="a name" ; // sera chargé depuis dans le localStorage ( après quand i ya aura l'API , ça ne sera plus mis dans le ocalstorage mais dans un state ainsi les données sont pas accessibles par l'utilisateur)
-    const[curveChartData,setCurveChartData]=useState(null);
-    useEffect(()=>{
-        const curveData=updateCurveChart(profile);
-        setCurveChartData(curveData)
-    },[profile]);
-    
-    const curveChartOption={
-        scales:{
-          y:{
-            beginAtZero:true
+    const curveChartRef = useRef(null);
+    const profile = "diakité";
+    const [curveChartData, setCurveChartData] = useState(null);
+
+    useEffect(() => {
+        const curveData = updateCurveChart(profile);
+        setCurveChartData(curveData);
+    }, [profile]);
+
+    const curveChartOption = {
+        scales: {
+          y: {
+            beginAtZero: true
           }
-          
         }
-      }
-    
-  return (
-    <>
-      <section className="w-full h-fit px-4">
-        <div id="evolution" className="w-full h-fit flex flex-col gap-4">
-          <h2 className="font-semibold text-[20px]">Votre évolution</h2>
-          <div className="w-[50%] h-full text-content font-content bg-white rounded-lg px-3 py-3">
-            {/* {chartData && <Bar ref={chartRef} data={chartData} options={options} />} */}
-            {barChartData ? (
-              <Line
-                ref={curveChartRef}
-                data={curveChartData}
-                options={curveChartOption}
-              />
-            ) : (
-              <p>Aucune données trouvées pour {profile}</p>
-            )}
-          </div>
-        </div>
-      </section>
-    </>
-  );
+    };
+
+    return (
+        <>
+            <section className="w-full h-fit px-4 flex flex-col gap-4">
+                <div id="evolution" className="w-full h-fit flex flex-col gap-4">
+                    <h2 className="font-semibold text-[20px]">Votre évolution</h2>
+                    <div className="w-[90%] h-fit text-content font-content bg-white rounded-lg px-4 py-4">
+                        {curveChartData ? (
+                            <Line
+                                ref={curveChartRef}
+                                data={curveChartData}
+                                options={curveChartOption}
+                            />
+                        ) : (
+                            <p>Aucune donnée trouvée pour {profile}</p>
+                        )}
+                    </div>
+                </div>
+
+                <div id="leaderboard">
+                  <h2 className="font-semibold text-[20px]">Classement</h2>
+
+                </div>
+
+
+            </section>
+        </>
+    );
 }
 
 export default OverView;
